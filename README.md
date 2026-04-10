4b802c4e15ec8deaf7b859fc50d1c0f3c72a72ad
AlejandroGuinel <alejandroguinel@hotmail.com>
2026-04-10 02:29:53 +0000

    ecommerce.1

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ecommerce-python
+
+## datos:
+- catalogo de productos :   
+    `productos = {
+        "id": 1,  #int
+        "nombre":"carlos", #str
+        "categoria":"tecnologia", #str
+        "precio": 100 #int
+    } 
+
+
+logica 
+
+- mostrar catalogo de productos
+- agragar productos a carrito
+- calcular total del carrito
---
+++ menu/menu.py
@@ -0,0 +1,33 @@
+import questionary
+
+
+def mostrar_menu_principal():
+    return questionary.select(
+        "Selecciona una opción:",
+        choices=[
+            "Ver menú",
+            "Buscar plato",
+            "Agregar al carrito",
+            "Ver carrito",
+            "Vaciar carrito",
+            "Salir"
+        ]
+    ).ask()
+
+
+def pedir_texto(mensaje):
+    return questionary.text(mensaje).ask()
+
+
+def pedir_numero(mensaje):
+    valor = questionary.text(mensaje).ask()
+    return int(valor)
+
+
+def mostrar_platos(platos):
+    if not platos:
+        print("No hay resultados")
+        return
+
+    for p in platos:
+        print(f"{p['id']} - {p['nombre']} (${p['precio']}) [{p['categoria']}]")
